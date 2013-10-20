27bd12eeb84badf6393dde64d20e103e746f6631
Bache <basselop@gmail.com>
2013-10-20 11:42:55 +0200

    Oauth cleanup

--- app/oauth/OauthClient.scala
+++ app/oauth/OauthClient.scala
@@ -14,6 +14,7 @@ import controllers.UserFinder
 trait Decoder[T] {
   type Req = Request[AnyContent]
   def cookieName: String
+  def cookieValue(t: T): String
   
   /** Looks in MongoDb for a user with T-specific credentials */
   def validUser[T : UserFinder](t: T): Future[Boolean] = MongoAdapter.userExists(t)
@@ -24,29 +25,35 @@ trait Decoder[T] {
   }
   
   /** Returns the cookie that is set when init is done */
-  def initUserData(email: String, code: String): Future[Cookie]
+  def initUserData(email: String, code: String)(implicit uf: UserFinder[T]): Future[Cookie] = for {
+    Some(user) <- getUserData(email, code)
+    lasterror <- MongoAdapter.addOauth(email, user)
+    if lasterror.ok
+  } yield Cookie(cookieName, cookieValue(user))
+  
+  /** Abstract that returns a future oauthuser */
+  def getUserData(email: String, code: String): Future[Option[T]]
 }
 
 object FacebookDecoder extends Decoder[FbUser] {
   def cookieName = "fb"
+    
+  def cookieValue(f: FbUser) = f.facebook_id
   
-  def initUserData(email: String, code: String) = for { 
-    Some(fbuser) <- getFbData(code, s"http://skandal.dyndns.tv:9000/users/$email/fblogin")
-    lasterror <- MongoAdapter.setFb(email=email, fbuser)
-    if lasterror.ok
-  } yield Cookie(cookieName, fbuser.facebook_id)
-
-  private def getFbData(code: String, redirect_uri: String): Future[Option[FbUser]] = future {
+  def getUserData(email: String, code: String): Future[Option[FbUser]] = future {
+    val redirect_uri = s"http://skandal.dyndns.tv:9000/users/$email/fblogin"
     val params = List("redirect_uri" -> redirect_uri,
       "client_secret" -> "55093193de6f163ddf4825f0a81de170",
       "client_id" -> "184407735081979",
       "scope" -> "user_friends",
       "code" -> code)
     for {
+      // first get access token
       AccessTokenBody(accesskey, expires) <- getExternalWs(s"https://graph.facebook.com/oauth/access_token?", params: _*)
-      userData <- getExternalWs(s"https://graph.facebook.com/me?", "access_token" -> accesskey) 
+      // then get userData
+      userData <- getExternalWs(s"https://graph.facebook.com/me?", "access_token" -> accesskey)
+      // then parse the json to an FbUser
       fbUser <- userData.fromJson[FbUser]
     } yield fbUser
   }
-
 }
